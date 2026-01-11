8d348342de999a707b78c2ea6aeb835c792ea511
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-01-11 05:59:21 +0200

    Implement 99 Bottles of Beer song logic

---
+++ loops/99 Bottles.py
@@ -0,0 +1,10 @@
+# for i in range(99):
+#  i = 99 - i
+#  print(f'{i} bottles of beer on the wall')
+#  print(f'{i} bottles of beer')
+#  print('Take one down, pass it around')
+
+for i in range(99, 0, -1):
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print('Take one down, pass it around')
