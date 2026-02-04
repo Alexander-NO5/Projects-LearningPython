e9f7fe2b476b2526239e0e4a3a0b63c66d4fd113
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-02-04 07:35:49 +0200

    Add fruit set operations in Fruit Cart.py

---
+++ data_structures/🍉 Fruit Cart.py
@@ -0,0 +1,13 @@
+my_fruits = {'banana', 'orange', 'cantaloupe'}
+
+friend_fruits = {'banana', 'grape', 'kiwi'}
+
+union_result = my_fruits.union(friend_fruits)
+
+intersection_result = my_fruits.intersection(friend_fruits)
+
+difference_result = my_fruits.difference(friend_fruits)
+
+print(union_result)
+print(intersection_result)
+print(difference_result)
