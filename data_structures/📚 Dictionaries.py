133c781dc984696864f5a60c64ca35c0a78f6bd5
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-02-04 04:28:56 +0200

    Add dictionary methods to display keys, values, items

--- data_structures/📚 Dictionaries.py
+++ data_structures/📚 Dictionaries.py
@@ -9,3 +9,8 @@ print('Grade:', student_info['grade'])
 # Modifying dictionary elements
 student_info['age'] = 10
 print('Updated Age:', student_info['age'])
+
+# Dictionary methods
+print('Keys:', student_info.keys())
+print('Values:', student_info.values())
+print('Items:', student_info.items())
